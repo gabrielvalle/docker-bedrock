9f796694e140ad64b17d8868cd266b3c4c5608e6
Andreas Ek <andreas@andreasek.se>
2016-01-13 21:56:36 +0100

    Message to deploy.php creation

--- src/Installer.php
+++ src/Installer.php
@@ -14,15 +14,18 @@ class Installer
         $deployContent = file_get_contents(realpath(__DIR__.'/../config/deploy.php'));
         $deployContent = str_replace('the_project.dev', '', $deployContent);
 
-        $deployContent = str_replace(
-        	['the_project.dev','TIMEZONE'],
-        	[basename($projectDir).'.dev',date_default_timezone_get()],
-        	$deployContent
-        );
-
         if( !file_exists(projectDir.'/deploy.dep')) {
+
+	        $deployContent = str_replace(
+	        	['the_project.dev','TIMEZONE'],
+	        	[basename($projectDir).'.dev',date_default_timezone_get()],
+	        	$deployContent
+	        );
+
+        	echo 'Adding deploy.php in project root';
 			file_put_contents($projectDir.'/deploy.php', $deployContent);
         }
+        
     }
 
 }
