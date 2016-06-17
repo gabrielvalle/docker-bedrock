9075a768a30dd8265ce19e0ee323533791502a4b
Hendrik Maus <aidentailor@gmail.com>
2016-06-17 15:15:26 +0200

    The installer used two different file names for the deployer file

--- src/DockerBedrock/Installer.php
+++ src/DockerBedrock/Installer.php
@@ -12,16 +12,17 @@ class Installer
         $projectDir = realpath($vendorDir.'/../');
         $deployContent = file_get_contents(realpath(__DIR__.'/../../config/deploy.php'));
         $deployContent = str_replace('the_project.dev', '', $deployContent);
+        $deployerFile = 'deploy.php';
 
-        if (!file_exists($projectDir.'/deploy.dep')) {
+        if (!file_exists($projectDir.'/'.$deployerFile)) {
             $deployContent = str_replace(
                 ['the_project.dev', 'TIMEZONE'],
                 [basename($projectDir).'.dev', date_default_timezone_get()],
                 $deployContent
             );
 
-            echo 'Adding deploy.php in project root';
-            file_put_contents($projectDir.'/deploy.php', $deployContent);
+            echo sprintf('Adding %s to the project root', $deployerFile);
+            file_put_contents($projectDir.'/'.$deployerFile, $deployContent);
         }
     }
 }
